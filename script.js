ce068212ab4e5fc57a3153cc43b96310977f7f4b
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-21 09:42:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-//your code here
+let bandNames = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
 
+function stripArticle(name) {
+    return name.replace(/^(a |an |the )/i, '').trim();
+}
+
+bandNames.sort((a, b) => stripArticle(a).localeCompare(stripArticle(b)));
+
+const ul = document.getElementById('bands');
+bandNames.forEach(name => {
+    const li = document.createElement('li');
+    li.textContent = name;
+    ul.appendChild(li);
+});
